4ffb3a3254952e43d9b3e80aec03c4e79aa54e26
柯刚 <Mrkegang@outlook.com>
2023-04-12 21:10:46 +0800

    Mrkegang, feat: 后端进行 SpringSecurity 基本配置

---
+++ study-project-backend/src/main/java/com/kgstudy/config/SecurityConfiguration.java
@@ -0,0 +1,76 @@
+package com.kgstudy.config;
+
+import com.alibaba.fastjson.JSONObject;
+import com.kgstudy.entity.RestBean;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
+
+/**
+ * @author kg
+ * @version 1.0
+ * @description
+ * @date 2023/4/12 20:07
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/api/auth/login")
+                .successHandler(this::onAuthenticationSuccess)
+                .failureHandler(this::onAuthenticationFailure)
+                .and()
+                .logout()
+                .logoutUrl("/api/auth/logout")
+                .and()
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(this::onAuthenticationFailure)
+                .and()
+                .build();
+    }
+
+    /**
+     * 用SpringSecuriy自定义登录成功
+     * @param request
+     * @param response
+     * @param authentication
+     * @throws IOException
+     * @throws ServletException
+     */
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(JSONObject.toJSONString(RestBean.success("登录成功！")));
+    }
+
+    /**
+     * 用SpringSecuriy自定义登录失败
+     * @param request
+     * @param response
+     * @param exception
+     * @throws IOException
+     * @throws ServletException
+     */
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(JSONObject.toJSONString(RestBean.failure(401,exception.getMessage())));
+    }
+
+}
